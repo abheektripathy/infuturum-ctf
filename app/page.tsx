9f052921b3a00aca67b8e0e202128ab2fcf9f2b0
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-02-09 04:02:19 +0530

    hotfix

--- app/page.tsx
+++ app/page.tsx
@@ -33,16 +33,16 @@ export default function Home() {
   
   <div className="absolute inset-0 mb-10 flex items-center justify-center">
     <li className="content-center">
-    <h1 className="text-white text-6xl md:text-7xl lg:text-8xl xl:text-9xl sm:text-4xl font-bold font-poppins text-center mb-10 ml-5 mr-5"  style={{
+    <h1 className="text-white text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold font-poppins text-center mb-10 ml-5 mr-5"  style={{
         background: 'linear-gradient(to right,rgba(114, 231, 23, 1), rgba(1, 70, 0, 1))',
         backgroundClip: 'text',
         WebkitBackgroundClip: 'text',
         WebkitTextFillColor: 'transparent',
       }}>infuturum.acm</h1>
     
-    <h1 className="text-white text-lg md:text-xl  font-mono text-center sm:text-sm" >     Embark on a journey to the future with Infuturum  </h1>
-    <h1 className="text-white text-lg md:text-xl font-mono text-center sm:text-sm" >    exploring the latest in technology, learning.</h1>
-    <h2 className="text-white text-lg md:text-xl font-mono text-center sm:text-sm " ></h2>
+    <h1 className="text-white text-lg md:text-xl  font-mono text-center " >     Embark on a journey to the future with Infuturum  </h1>
+    <h1 className="text-white text-lg md:text-xl font-mono text-center " >    exploring the latest in technology, learning.</h1>
+    <h2 className="text-white text-lg md:text-xl font-mono text-center " ></h2>
     <div className="flex items-center justify-center mt-10">
   <img src="/trycode.png" alt="Image" className={`w-1/3 h-1/3 xs:w-2/3 xs:h-2/3 md:w-2/3 md:h-2/3 lg:w-1/3 lg:h-1/3 xl:w-1/3 xl:h-1/3`}   />a
 </div>
