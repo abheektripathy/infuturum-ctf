8f2fc5ee829b881f7efa82af25560c335b6eb28b
revv <asijaprakash27@gmail.com>
2023-02-20 19:46:24 +0530

    fixed horizontal off-screen overflow

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -40,11 +40,11 @@ const Timeline: React.FC = () => {
       </div>
     
 
-      <div className="relative h-screen w-screen bg-black">
+      <div className="relative h-full w-full bg-black">
         <br></br>
         <img
     src="/ring.png"
-    className="absolute h-full w-full object-cover opacity-20"
+    className="absolute h-100% w-100% object-cover opacity-20"
   />
 
         <h1
